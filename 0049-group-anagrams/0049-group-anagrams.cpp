a1dd66066f8c0b14fce48ec1af6d001502198a4f
Aokman12 <128150490+Aokman12@users.noreply.github.com>
2023-05-02 23:28:26 +0530

    Time: 36 ms (72.03%), Space: 20.7 MB (45.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.size() == 1){
+            return {{strs[0]}};
+        }
+        
         vector<vector<string>> ans;
         unordered_map<string,vector<string>> mp;
         

9b944d591924f43fe47faf335022c173692af41e
Aokman12 <128150490+Aokman12@users.noreply.github.com>
2023-05-27 07:43:52 +0530

    Time: 3 ms (93.93%), Space: 13.8 MB (25.81%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -16,10 +16,23 @@ public:
         return dp[n];
     }
     
+    int solve2(int n,vector<int> &cost){
+        vector<int> dp(n+1);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+        }
+        
+        return min(dp[n-1],dp[n-2]);
+    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n+1,-1);
-        int ans = min(solve(cost,n-1,dp),solve(cost,n-2,dp));
-        return ans;
+        // vector<int> dp(n+1,-1);        
+        // int ans = min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        return solve2(n,cost);
     }
 };

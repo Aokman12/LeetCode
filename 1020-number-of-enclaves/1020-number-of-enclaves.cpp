2c2c24c5c241faca8afa4e6fdb884f98de7f672b
Aokman12 <128150490+Aokman12@users.noreply.github.com>
2023-06-08 13:04:50 +0530

    Time: 80 ms (64.71%), Space: 27.6 MB (85.56%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,50 +1,45 @@
 class Solution {
-private:
-    void dfs(int row,int col,vector<vector<int>> &visited,vector<vector<int>> &grid,int delrow[],int delcol[]){
-        visited[row][col] = 1;
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        for(int i=0;i<4;i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
-                dfs(nrow,ncol,visited,grid,delrow,delcol);
-            }
-        }
-    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         
+        queue<pair<int,int>> q;
         vector<vector<int>> visited(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j] == 1){
+                        visited[i][j] = 1;
+                        q.push({i,j});
+                    }
+                }
+            }
+        }
         
         int delrow[] = {-1,0,1,0};
         int delcol[] = {0,1,0,-1};
         
-        for(int j=0;j<m;j++){
-            if(!visited[0][j] && grid[0][j] == 1){
-                dfs(0,j,visited,grid,delrow,delcol);
-            }
-            if(!visited[n-1][j] && grid[n-1][j] == 1){
-                dfs(n-1,j,visited,grid,delrow,delcol);
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i][0] && grid[i][0] == 1){
-                dfs(i,0,visited,grid,delrow,delcol);
-            }
-            if(!visited[i][m-1] && grid[i][m-1] == 1){
-                dfs(i,m-1,visited,grid,delrow,delcol);
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
+                    visited[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
+                }
             }
         }
         
         int count = 0;
-        
+            
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!visited[i][j] && grid[i][j] == 1){
